bb8a46bef83536dbf318f6e16546b2740b9f8783
Tonzzu <t.o.ruusunen@gmail.com>
2020-07-10 19:25:21 +0300

    4.15 valmis

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 mongoose
   .connect(config.MONGO_URI, {
@@ -19,6 +20,8 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
